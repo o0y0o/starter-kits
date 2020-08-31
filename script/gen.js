c69447f3360856cb8c619c3b3e99632f8246afc6
Jason Chung <jason.chung@skylinetw.com>
2020-08-31 09:44:05 +0800

    fix script

--- script/gen.js
+++ script/gen.js
@@ -1,6 +1,8 @@
+#!/usr/bin/env node
+
 const path = require('path')
 const { runner } = require('hygen/dist/index')
-const Logger = require('hygen/dist/logger')
+const Logger = require('hygen/dist/logger').default
 
 runner(process.argv.slice(2), {
   templates: path.join(__dirname, '../__templates'),
